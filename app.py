fe341ece9f377f3c4c32a5136d9e8ec1affc950c
Trent Smith <trent.smith@chanzuckerberg.com>
2019-03-28 13:34:37 -0700

    test fix

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def serve_openid_config():
                          jwks_uri=f"https://{auth_host}/.well-known/jwks.json",
                          revocation_endpoint=f"https://{auth_host}/oauth/revoke",
                          userinfo_endpoint=f"https://{auth_host}/userinfo")
-    return openid_config
+    return Response(status_code=200, body=openid_config)
 
 
 def proxy_response(dest_url, **extra_query_params):
