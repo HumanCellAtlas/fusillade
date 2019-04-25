e468e74136c3dc50d54533811e8bf08699e0eae2
Harini Jagadeesan <harini.jagadeesan@chanzuckerberg.com>
2019-04-25 15:54:46 -0700

    Script to cleanup test directories and schemas

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -1,5 +1,9 @@
 import boto3
 from fusillade.clouddirectory import cleanup_directory, cleanup_schema
+"""
+This script is used to clean up test directories and schemas from aws clouddirectory
+"""
+
 client = boto3.client("clouddirectory")
 
 response = client.list_directories(
@@ -7,9 +11,9 @@ response = client.list_directories(
     state='ENABLED'
 )
 
-test_directories = [directory['DirectoryArn'] for directory in response['Directories'] if 'test' in directory['Name']]
-for directory in test_directories:
-    cleanup_directory(directory)
+for directory in response['Directories']:
+    if 'test' in directory['Name']:
+        cleanup_directory['DirectoryArn']
 
 directories = [ i['Name'] for i in client.list_directories(
     MaxResults=30,
@@ -17,7 +21,17 @@ directories = [ i['Name'] for i in client.list_directories(
 )['Directories']]
 print('DIRECTORIES:', directories)
 
+response = client.list_published_schema_arns(
+    MaxResults = 20
+)
+for schema in response['SchemaArns']:
+    if "authz/T" in schema:
+        cleanup_schema(schema)
 
+response = client.list_published_schema_arns(
+    MaxResults = 20
+)['SchemaArns']
+print('Schemas:',response)
 
 
 
