f3954cc5970a9b43b3cf2721c62fbc4d47f24a83
Trent Smith <trent.smith@chanzuckerberg.com>
2019-05-01 10:21:56 -0700

    Fixing cleanup script

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -1,34 +1,29 @@
-import boto3
-from fusillade.clouddirectory import cleanup_directory, cleanup_schema
+from fusillade.clouddirectory import cleanup_directory, cleanup_schema, cd_client
 """
 This script is used to clean up test directories and schemas from aws clouddirectory
 """
 
-client = boto3.client("clouddirectory")
 
-response = client.list_directories(
-    MaxResults=30,
-    state='ENABLED'
-)
+if __name__ == "__main__":
+    for response in cd_client.get_paginator('list_directories').paginate(MaxResults=30, state='ENABLED'):
+        for directory in response['Directories']:
+            if 'test' in directory['Name']:
+                cleanup_directory(directory['DirectoryArn'])
 
-for directory in response['Directories']:
-    if 'test' in directory['Name']:
-        cleanup_directory['DirectoryArn']
+    directories = [
+        i['Name'] for i in cd_client.list_directories(
+            MaxResults=30,
+            state='ENABLED'
+        )['Directories']
+    ]
+    print('DIRECTORIES:', directories)
 
-directories = [ i['Name'] for i in client.list_directories(
-    MaxResults=30,
-    state='ENABLED'
-)['Directories']]
-print('DIRECTORIES:', directories)
+    for response in cd_client.get_paginator('list_published_schema_arns').paginate(MaxResults=30):
+        for schema in response['SchemaArns']:
+            if "authz/T" in schema:
+                cleanup_schema(schema)
 
-response = client.list_published_schema_arns(
-    MaxResults = 20
-)
-for schema in response['SchemaArns']:
-    if "authz/T" in schema:
-        cleanup_schema(schema)
-
-response = client.list_published_schema_arns(
-    MaxResults = 20
-)['SchemaArns']
-print('Schemas:',response)
+    response = cd_client.list_published_schema_arns(
+        MaxResults=30
+    )['SchemaArns']
+    print('Schemas:', response)
