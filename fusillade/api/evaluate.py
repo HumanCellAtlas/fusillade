3f8e326555f3e07b88aded8e09b28d1d64236305
Trent Smith <trent.smith@chanzuckerberg.com>
2019-05-02 14:44:53 -0700

    Adding more documentation
    - moved threading code into AuthorizateThread Class

--- fusillade/api/evaluate.py
+++ fusillade/api/evaluate.py
@@ -5,37 +5,20 @@ import typing
 from flask import make_response, jsonify
 
 from fusillade import User, directory
-
 from fusillade.utils.authorize import assert_authorized, evaluate_policy
 
 
 def evaluate_policy_api(user, body):
-    with ThreadedAuthorize(user, ['fus:Evaluate'], ['arn:hca:fus:*:*:user']):
+    with AuthorizeThread(user, ['fus:Evaluate'], ['arn:hca:fus:*:*:user']):
         policies = User(directory, body['principal']).lookup_policies()
         result = evaluate_policy(body['principal'], body['action'], body['resource'], policies)
     return make_response(jsonify(**body, result=result), 200)
 
 
-def call_with_future(fn, future, args, kwargs):
-    try:
-        result = fn(*args, **kwargs)
-        future.set_result(result)
-    except Exception as exc:
-        future.set_exception(exc)
-
-
-def threaded(fn):
-    def wrapper(*args, **kwargs):
-        future = Future()
-        Thread(target=call_with_future, args=(fn, future, args, kwargs)).start()
-        return future
-
-    return wrapper
-
-
-class ThreadedAuthorize:
+class AuthorizeThread:
     """
-    Allows us to authorize the request and evaluate the request in parallel.
+    Authorize the requester in a separate thread while executing the request. This is safe only when performing
+    read operations. If authorization fails a 403 is returned and the original request results are discarded.
     """
     def __init__(
             self,
@@ -43,16 +26,26 @@ class ThreadedAuthorize:
             actions: typing.List[str],
             resources: typing.List[str]
     ):
-        self.user = user
-        self.actions = actions
-        self.resources = resources
+        self.args = (user, actions, resources)
 
     def __enter__(self):
-        self.future = self._evaluate()
+        self.future = self.evaluate()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.future.result()
 
-    @threaded
-    def _evaluate(self):
-        assert_authorized(self.user, self.actions, self.resources)
+    @staticmethod
+    def _call_with_future(fn, future, args):
+        """
+        Returns the result of the wrapped threaded function.
+        """
+        try:
+            result = fn(*args)
+            future.set_result(result)
+        except Exception as exc:
+            future.set_exception(exc)
+
+    def evaluate(self):
+        future = Future()
+        Thread(target=self._call_with_future, args=(assert_authorized, future, self.args)).start()
+        return future
