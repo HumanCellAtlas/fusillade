fe00967f59016afeeb820aebb93341118cce5ff5
Trent Smith <trent.smith@chanzuckerberg.com>
2019-11-07 16:11:24 -0800

    fixing tests

--- tests/json_mixin.py
+++ tests/json_mixin.py
@@ -6,3 +6,14 @@ class AssertJSONMixin:
 
     def assertJSONEqual(self, expected, actual, *args, **kwargs):
         self.assertEqual(normalize_json(expected), normalize_json(actual), *args, **kwargs)
+
+    def assertJSONListEqual(self, expected, actual, *args, **kwargs):
+        "check if two lists of json objects are equal."
+        expected = set([normalize_json(i) for i in expected])
+        actual = set([normalize_json(i) for i in actual])
+        self.assertEqual(expected, actual, *args, **kwargs)
+
+    def assertJSONIn(self, member, group, *args, **kwargs):
+        group = set([normalize_json(i) for i in group])
+        member = normalize_json(member)
+        self.assertIn(member, group, *args, **kwargs)
