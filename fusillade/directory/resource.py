aadd82c4ecf09f67f4a52f8b4eaf52363c26f0d8
Trent Smith <trent.smith@chanzuckerberg.com>
2019-11-20 10:35:15 -0800

    duplication

--- fusillade/directory/resource.py
+++ fusillade/directory/resource.py
@@ -180,13 +180,6 @@ class ResourceType(CloudNode):
             cls._resource_types = [name for name, _ in cd.list_object_children(f'/{cls.object_type}/')]
         return cls._resource_types
 
-    @classmethod
-    def get_types(cls):
-        if not cls._resource_types:
-            cd = Config.get_directory()
-            cls._resource_types = [name for name, _ in cd.list_object_children(f'/{cls.object_type}/')]
-        return cls._resource_types
-
     @staticmethod
     def hash_name(name):
         """
