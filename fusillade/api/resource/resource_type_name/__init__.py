a29468182a150f7d676d22f4642645b298b31add
Trent Smith <1429913+Bento007@users.noreply.github.com>
2019-11-25 13:41:34 -0800

    resource api create (#356)
    * Adding APIs for Create, Read, and Delete of Resource Types
    
    - Adding API to list all resource types
    - Adding API to create, read, and delete resource types
    - Fixing typo when getting resource type info
    - A resource type cannot be deleted if there are project ids connected to the resource.

---
+++ fusillade/api/resource/resource_type_name/__init__.py
@@ -0,0 +1,42 @@
+from flask import request, make_response, jsonify
+
+from fusillade.directory.resource import ResourceType
+from fusillade.utils.authorize import authorize
+
+
+@authorize(["fus:GetResources"],
+           ["arn:hca:fus:*:*:resource/{resource_type_name}"],
+           resource_params=['resource_type_name'])
+def get(token_info: dict, resource_type_name):
+    rt = ResourceType(resource_type_name)
+    rt._exists([rt.name])
+    info = rt.get_info()
+    return make_response(info, 200)
+
+
+@authorize(["fus:PostResources"],
+           ["arn:hca:fus:*:*:resource/{resource_type_name}"],
+           resource_params=['resource_type_name'])
+def post(token_info: dict, resource_type_name):
+    json_body = request.json
+    rt = ResourceType.create(resource_type_name, json_body['actions'])
+    return make_response(f"New resource type {rt.name} created.", 201)
+
+
+@authorize(["fus:DeleteResources"],
+           ["arn:hca:fus:*:*:resource/{resource_type_name}"],
+           resource_params=['resource_type_name'])
+def delete(token_info: dict, resource_type_name):
+    rt = ResourceType(resource_type_name)
+    rids = rt.list_ids()[0]
+    if len(rids) > 0:
+        # TODO write tests for this once create resource_id API is complete
+        return make_response(jsonify(
+            {'msg': 'All resource ids must be deleted before the resource type can be deleted.',
+             'resource_ids': rids}), 409)
+    rt.delete_node()
+    return make_response(jsonify({
+        'msg': "Resource type deleted.",
+        'resource_type': rt.name}),
+        200
+    )
