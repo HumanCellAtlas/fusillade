56b4ba9ed7486abf4c31238edfd9273cf684de61
Trent Smith <trent.smith@chanzuckerberg.com>
2019-12-05 11:31:16 -0800

    Fix auto user provisioning
    - Provision the user for evaluation requests, if they don't exist.
    - Also a random typing fix.
    - adding tests
    - sharing arn_prefix in test.

--- fusillade/directory/authorization.py
+++ fusillade/directory/authorization.py
@@ -1,5 +1,6 @@
 from typing import List, Union
 
+from dcplib.aws.clients import clouddirectory as cd_client
 from fusillade import Config
 from fusillade.directory import User, Group
 from fusillade.directory.resource import ResourceId, ResourceType
@@ -18,12 +19,18 @@ def get_resource_authz_parameters(user: str, resources: Union[List[str], str]):
     """
     policies = []
     _user = User(user)
+    try:
+        groups = _user.groups
+    except cd_client.exceptions.ResourceNotFoundException:
+        _user = User.provision_user(user)
+        groups = _user.groups
+
     # Only support a single resource for now
     resource = resources[0] if isinstance(resources, list) else resources
     r_type, r_id, *_ = resource.split(':')[-1].split('/')
     if r_type in ResourceType.get_types():
         r_id = ResourceId(r_type, r_id)
-        resource_policies = r_id.check_access([_user] + [Group(g) for g in _user.groups])
+        resource_policies = r_id.check_access([_user] + [Group(g) for g in groups])
         if not resource_policies:
             raise FusilladeForbiddenException()
         policies.extend(resource_policies)
